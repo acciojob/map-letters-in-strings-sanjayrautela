3eadcde44480c0897d4db92fc7bc275d94087f60
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-13 10:07:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapLetters(word){
+	const letterMap = {};
+
+	for(let i=0; i<word.length; i++){
+		const letter = word[i];
+
+		if(!lettermap[letter]){
+			letterMap[letter] = [i];
+		}
+		else{
+			letterMap[letter].push(i);
+		}
+	}
+	return letterMap;
+}
